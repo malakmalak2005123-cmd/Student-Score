9343257d548ef766882e76edf4b9555550b966b9
malakmalak2005123-cmd <malakmalak2005123@gmail.com>
2026-01-11 18:18:58 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+interface Student{
+    id:number;
+    name:string;
+    score:number;
+    isactive:boolean;
+}
+const student: Student = {
+  id: 1,
+  name: "Ali",
+  score: 14,
+  isactive: true,
+};
