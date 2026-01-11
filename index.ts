2416f3f8b6a24eddf2bcf846cf46598ed68ee6dd
malakmalak2005123-cmd <malakmalak2005123@gmail.com>
2026-01-11 18:40:54 +0100

    calcul avg score

--- index.ts
+++ index.ts
@@ -2,11 +2,16 @@ interface Student{
     id:number;
     name:string;
     score:number;
-    isactive:boolean;
+    isActive:boolean;
 }
-const student: Student = {
-  id: 1,
-  name: "Ali",
-  score: 14,
-  isactive: true,
-};
+const students: Student[] = [
+  { id: 1, name: "Ahmed", score: 15, isActive: true },
+  { id: 2, name: "Sara", score: 18, isActive: true },
+  { id: 3, name: "Yassine", score: 10, isActive: false }
+];
+
+
+function getAverageScore(students: Student[]): number {
+  const total = students.reduce((sum, student) => sum + student.score, 0);
+  return total / students.length;
+}
